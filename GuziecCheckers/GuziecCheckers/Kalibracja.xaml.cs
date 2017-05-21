ccca8a4a7444b63c5565c811903cf0c41296c057
Mattioo94 <mattioo@wp.pl>
2017-05-21 22:33:17 +0200

    - drobne zmiany w panelu kalibracji

--- GuziecCheckers/GuziecCheckers/Kalibracja.xaml.cs
+++ GuziecCheckers/GuziecCheckers/Kalibracja.xaml.cs
@@ -24,24 +24,24 @@ namespace GuziecCheckers
                     #region Aktualizacja informacji o kolorach pionów
                     int R = 0, G = 0, B = 0;
 
-                    P1Rmin.Dispatcher.Invoke(() => { R = Convert.ToInt32(P1Rmin.Text); });
-                    P1Gmin.Dispatcher.Invoke(() => { G = Convert.ToInt32(P1Gmin.Text); });
-                    P1Bmin.Dispatcher.Invoke(() => { B = Convert.ToInt32(P1Bmin.Text); });
+                    P1Rmin.Dispatcher.Invoke(() => { if (!string.IsNullOrEmpty(P1Rmin.Text)) R = Convert.ToInt32(P1Rmin.Text); });
+                    P1Gmin.Dispatcher.Invoke(() => { if (!string.IsNullOrEmpty(P1Gmin.Text)) G = Convert.ToInt32(P1Gmin.Text); });
+                    P1Bmin.Dispatcher.Invoke(() => { if (!string.IsNullOrEmpty(P1Bmin.Text)) B = Convert.ToInt32(P1Bmin.Text); });
                     Chessboard.PawnsInfo.minColorRange1 = new Bgr(R, G, B);
 
-                    P1Rmax.Dispatcher.Invoke(() => { R = Convert.ToInt32(P1Rmax.Text); });
-                    P1Gmax.Dispatcher.Invoke(() => { G = Convert.ToInt32(P1Gmax.Text); });
-                    P1Bmax.Dispatcher.Invoke(() => { B = Convert.ToInt32(P1Bmax.Text); });
+                    P1Rmax.Dispatcher.Invoke(() => { if (!string.IsNullOrEmpty(P1Rmax.Text)) R = Convert.ToInt32(P1Rmax.Text); });
+                    P1Gmax.Dispatcher.Invoke(() => { if (!string.IsNullOrEmpty(P1Gmax.Text)) G = Convert.ToInt32(P1Gmax.Text); });
+                    P1Bmax.Dispatcher.Invoke(() => { if (!string.IsNullOrEmpty(P1Bmax.Text)) B = Convert.ToInt32(P1Bmax.Text); });
                     Chessboard.PawnsInfo.maxColorRange1 = new Bgr(R, G, B);
 
-                    P2Rmin.Dispatcher.Invoke(() => { R = Convert.ToInt32(P2Rmin.Text); });
-                    P2Gmin.Dispatcher.Invoke(() => { G = Convert.ToInt32(P2Gmin.Text); });
-                    P2Bmin.Dispatcher.Invoke(() => { B = Convert.ToInt32(P2Bmin.Text); });
+                    P2Rmin.Dispatcher.Invoke(() => { if (!string.IsNullOrEmpty(P2Rmin.Text)) R = Convert.ToInt32(P2Rmin.Text); });
+                    P2Gmin.Dispatcher.Invoke(() => { if (!string.IsNullOrEmpty(P2Gmin.Text)) G = Convert.ToInt32(P2Gmin.Text); });
+                    P2Bmin.Dispatcher.Invoke(() => { if (!string.IsNullOrEmpty(P2Bmin.Text)) B = Convert.ToInt32(P2Bmin.Text); });
                     Chessboard.PawnsInfo.minColorRange2 = new Bgr(R, G, B);
 
-                    P2Rmax.Dispatcher.Invoke(() => { R = Convert.ToInt32(P2Rmax.Text); });
-                    P2Gmax.Dispatcher.Invoke(() => { G = Convert.ToInt32(P2Gmax.Text); });
-                    P2Bmax.Dispatcher.Invoke(() => { B = Convert.ToInt32(P2Bmax.Text); });
+                    P2Rmax.Dispatcher.Invoke(() => { if (!string.IsNullOrEmpty(P2Rmax.Text)) R = Convert.ToInt32(P2Rmax.Text); });
+                    P2Gmax.Dispatcher.Invoke(() => { if (!string.IsNullOrEmpty(P2Rmax.Text)) G = Convert.ToInt32(P2Gmax.Text); });
+                    P2Bmax.Dispatcher.Invoke(() => { if (!string.IsNullOrEmpty(P2Rmax.Text)) B = Convert.ToInt32(P2Bmax.Text); });
                     Chessboard.PawnsInfo.maxColorRange2 = new Bgr(R, G, B);
                 #endregion
 
